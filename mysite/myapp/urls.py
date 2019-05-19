8c898682a8a7141904efa26f667f5e31b40f0081
Mohammed Danish Shaikh <danish778866@gmail.com>
2019-05-19 15:25:32 -0500

    Clean clean_file view

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from myapp.views import upload, show_doc, choices, clean_file, downloads_page, sample
+from myapp.views import upload, show_doc, choices, download, downloads_page, sample, upload_cdrive
 
 urlpatterns = [
     url(r'^list/$', upload, name='upload'),
     url(r'^show_doc/$', show_doc, name='show_doc'),
     url(r'^choices/$', choices, name='choices'),
-    url(r'^clean_file/$', clean_file, name='clean_file'),
+    url(r'^download/$', download, name='download'),
     url(r'^downloads/$', downloads_page, name='downloads_page'),
     url(r'^sample/$', sample, name='sample'),
+    url(r'^upload_cdrive/$', upload_cdrive, name='upload_cdrive'),
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -25,6 +25,7 @@ CDRIVE_URL = "http://a7648f6f5702911e98ea412ac368fc7a-1169430973.us-east-1.elb.a
 CLIENT_TOKEN_KEY = "procleaner_token"
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 CDRIVE_FILES_DIR = os.path.join(PROJECT_DIR, "cdrive_files")
+CLEAN_FILES = os.path.join(os.path.join(os.path.join(os.path.join(PROJECT_DIR, "myapp"), "static"), "pdf"), "clean")
 CLEAN_FILES_LOCAL = os.path.join(os.path.join(os.path.join(os.path.join(PROJECT_DIR, "myapp"), "static"), "pdf"), "clean_local")
 CLEAN_FILES_CDRIVE = os.path.join(os.path.join(os.path.join(os.path.join(PROJECT_DIR, "myapp"), "static"), "pdf"), "clean_cdrive")
 CLEAN_FILES_SUFFIX = ".clean"
@@ -119,40 +120,48 @@ def choices(request):
     return render(request, 'profiler_choice.html', {'profiler_options': profiler_options})
 
 @csrf_exempt
-def clean_file(request):
-    uploaded_file_name = request.session.get('uploaded_file_path')
+def upload_cdrive(request):
+    uploaded_file = request.session.get('uploaded_file')
     if request.session["file_type"] == "CDrive":
-        uploaded_file_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + os.sep + "cdrive_files" + os.sep \
-                + uploaded_file_name
+        uploaded_file_path = os.path.join(CDRIVE_FILES_DIR, uploaded_file)
     else:
-        uploaded_file_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + uploaded_file_name
-    project_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    cleaned_file_path = project_dir + os.sep + "myapp" + os.sep + "static" + os.sep + "pdf" + os.sep + os.path.basename(uploaded_file_name) + ".clean"
-    if request.method == "POST":
-        files = {'file': open(cleaned_file_path, 'rb')}
-        cdrive_url = "http://a7648f6f5702911e98ea412ac368fc7a-1169430973.us-east-1.elb.amazonaws.com"
-        post_url = cdrive_url + "/upload/"
-        token = request.session["procleaner_token"]
-        header = {"Authorization": "Bearer " + token}
-        try:
-            r = requests.post(post_url, files=files, headers=header)
-            django_response = HttpResponse(
-                content=r.content,
-                status=r.status_code,
-                content_type=r.headers['Content-Type']
-            )
-        except:
-            django_response = HttpResponseBadRequest
-        return django_response
-    else:
-        A = pd.read_csv(uploaded_file_path)
-        A.to_csv(cleaned_file_path, index=False)
-        return render(request,
-                      'clean_file.html',
-                      {'clean_file_name': os.path.basename(uploaded_file_name) + ".clean",
-                       'clean_file_path': cleaned_file_path}
-                     )
+        uploaded_file_path = os.path.join(PROJECT_DIR, uploaded_file)
+    clean_file_name = os.path.basename(uploaded_file) + CLEAN_FILES_SUFFIX
+    clean_file_path = os.path.join(CLEAN_FILES, clean_file_name)
+    files = {'file': open(clean_file_path, 'rb')}
+    post_url = CDRIVE_URL + "/upload/"
+    token = request.session[CLIENT_TOKEN_KEY]
+    header = {"Authorization": "Bearer " + token}
+    try:
+        upload_response = requests.post(post_url, files=files, headers=header)
+        django_response = HttpResponse(
+            content=upload_response.content,
+            status=upload_response.status_code,
+            content_type=upload_response.headers['Content-Type']
+        )
+    except:
+        django_response = HttpResponseBadRequest
+    return django_response
 
+def download(request):
+    uploaded_file = request.session.get('uploaded_file')
+    if request.session["file_type"] == "CDrive":
+        uploaded_file_path = os.path.join(CDRIVE_FILES_DIR, uploaded_file)
+    else:
+        uploaded_file_path = os.path.join(PROJECT_DIR, uploaded_file)
+    clean_file_name = os.path.basename(uploaded_file) + CLEAN_FILES_SUFFIX
+    clean_file_path = os.path.join(CLEAN_FILES, clean_file_name)
+    if not os.path.exists(CLEAN_FILES):
+        os.makedirs(CLEAN_FILES)
+    uploaded_df = pd.read_csv(uploaded_file_path)
+    uploaded_df.to_csv(clean_file_path, index=False)
+    return render(request,
+                  'clean_file.html',
+                      {
+                          'clean_file_name': clean_file_name,
+                          'clean_file_path': clean_file_path
+                      }
+                 )
 
 def show_doc(request):
     if request.session["file_type"] == "CDrive":
