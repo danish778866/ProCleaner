a38394674abdbf56d27e3861fc0929d4811f77c6
Mohammed Danish Shaikh <danish778866@gmail.com>
2019-05-22 14:12:53 -0500

    Add comments

--- mysite/utils/utils.py
+++ mysite/utils/utils.py
@@ -3,6 +3,20 @@ import py_stringmatching as sm
 import pandas as pd
 
 def get_similar_strings(table_path):
+    """
+    Get list of strings to be normalized by value normalizer.
+    The current algorithm is as follows:
+        1. Deduplicate the given list of strings.
+        2. Apply string similar join.
+        3. Retrive the top N similar strings as returned by string similar join.
+    Arguments:
+        table_path: The absolute path of list of strings.
+    Returns:
+        similar_strings: Similar strings as returned by the aforementioned algorithm.
+
+    Note: This logic can be changed to improve the value normalizer part of the
+          overall application.
+    """
     A = pd.read_csv(table_path)
     B = pd.read_csv(table_path)
     qg3_tok = sm.QgramTokenizer(qval=3)
